9263a4b0a4e75fb4c3eeb0e60840e9957b03cd41
Tate, Hongliang Tian <tatetian@gmail.com>
2018-11-18 23:03:28 +0800

    Works!

--- src/internal/occlum/syscall.s
+++ src/internal/occlum/syscall.s
@@ -2,12 +2,5 @@
 .hidden __syscall
 .type __syscall,@function
 __syscall:
-	movq %rdi,%rax
-	movq %rsi,%rdi
-	movq %rdx,%rsi
-	movq %rcx,%rdx
-	movq %r8,%r10
-	movq %r9,%r8
-	movq 8(%rsp),%r9
-	call __occlum_syscall@PLT
+	jmp __occlum_syscall@PLT
 	ret
