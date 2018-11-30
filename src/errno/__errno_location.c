109aefec381c23a0ad4bf0857a32bbc830c52563
Tate, Hongliang Tian <tatetian@gmail.com>
2018-11-30 16:35:19 +0800

    Add printf and all its dependencies

--- src/errno/__errno_location.c
+++ src/errno/__errno_location.c
@@ -1,9 +1,22 @@
 #include <errno.h>
 #include "pthread_impl.h"
 
+#ifndef __OCCLUM
+
 int *__errno_location(void)
 {
 	return &__pthread_self()->errno_val;
 }
+
+#else
+
+static int main_errno = 0;
+
+int *__errno_location(void)
+{
+	return &main_errno;
+}
+
+#endif /* __OCCLUM */
 
 weak_alias(__errno_location, ___errno_location);
--- src/stdio/__lockfile.c
+++ src/stdio/__lockfile.c
@@ -3,6 +3,7 @@
 
 int __lockfile(FILE *f)
 {
+#ifndef __OCCLUM
 	int owner = f->lock, tid = __pthread_self()->tid;
 	if ((owner & ~MAYBE_WAITERS) == tid)
 		return 0;
@@ -14,10 +15,15 @@ int __lockfile(FILE *f)
 			__futexwait(&f->lock, owner|MAYBE_WAITERS, 1);
 	}
 	return 1;
+#else
+    return 0;
+#endif
 }
 
 void __unlockfile(FILE *f)
 {
+#ifndef __OCCLUM
 	if (a_swap(&f->lock, 0) & MAYBE_WAITERS)
 		__wake(&f->lock, 1, 1);
+#endif
 }
