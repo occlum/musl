8ffb657ba856b0554e99c8f643ca23117fcb1c0e
Youren <shenyouren@gmail.com>
2019-04-03 09:27:32 +0800

    Fix mirror bug at C runtime library

--- crt/x86_64/crtn.s
+++ crt/x86_64/crtn.s
@@ -1,15 +1,18 @@
 .section .init
 	pop %rax
-  pop %r11
+  popq %r11
   movq (%r11), %r10
   bndcu %r10, %bnd2
   bndcl %r10, %bnd2
+  jmp *%r11
 	/* ret */
 
 .section .fini
+  nopl 512(%rbx,%rbx)
 	pop %rax
   pop %r11
   movq (%r11), %r10
   bndcu %r10, %bnd2
   bndcl %r10, %bnd2
+  jmp *%r11
 	/* ret */
