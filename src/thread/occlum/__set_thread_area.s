f45fe1d2f573c41897d15a3624aed07e3e730874
Tate, Hongliang Tian <tatetian@gmail.com>
2019-04-06 11:21:12 +0800

    Make sure %rsp is 16-byte aligned before call

--- src/thread/occlum/__set_thread_area.s
+++ src/thread/occlum/__set_thread_area.s
@@ -6,6 +6,9 @@ __set_thread_area:
     // Func args:
     //  void* addr - %rdi
 
+    // Make %rsp 16-aligned before call
+    sub $8, %rsp
+
     // Pass addr
     mov %rdi, %rdx
     // Pass SET_FS
@@ -17,4 +20,8 @@ __set_thread_area:
     //  int code    - %rsi
     //  void* addr  - %rdx
     call __syscall
+
+    // Restore %rsp
+    add $8, %rsp
+
     ret
