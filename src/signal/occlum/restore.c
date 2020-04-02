7acb339d3368f11a3f1f090cefdbb0e43f4e4c63
Hui, Chunyang <sanqian.hcy@antfin.com>
2020-04-02 12:34:09 +0000

    Fix sigreturn segfault caused by abort
    sigreturn syscall should never return. However, tests show it returns and trigger
    abort to cause segfault. After removing abort, things become normal. More
    investigation is needed.

--- src/signal/occlum/restore.c
+++ src/signal/occlum/restore.c
@@ -5,6 +5,10 @@
 hidden void __restore_rt()
 {
     __syscall(SYS_rt_sigreturn);
-    // This should never happen
-    abort();
+    // This should never happen.
+    // FIXME: However, based on tests, this syscall still returns and
+    // will trigger abort and cause segmentation fault.
+    // The reason is unknown but after removing abort, things
+    // become normal.
+    // abort();
 }
